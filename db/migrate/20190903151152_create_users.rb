378ab78d10cf217a88af7fef21f391ef2b498cc8
rrankit <rankit.ranjan.contractor@move.com>
2019-09-03 21:11:06 +0530

    setup devise gem

--- db/migrate/20190903151152_create_users.rb
+++ db/migrate/20190903151152_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first
       t.string :last
-      t.string :email
+      t.string :email,              null: false, default: ""
 
       t.timestamps
     end
